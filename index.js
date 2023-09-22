423ac8d099258cc507143dcf67cbb6e32e46d3cd
Christopher <csbroox@gmail.com>
2023-09-22 04:51:16 -0500

    safety push

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const pg = require('pg');
-const client = new pg.Client('postgres://localhost/fullstack_template_db');
+const client = new pg.Client('postgres://localhost/movie-list');
 const express = require('express');
 const app = express();
 const path = require('path');
+app.use(express.json())
 
 const homePage = path.join(__dirname, 'index.html');
 app.get('/', (req, res)=> res.sendFile(homePage));
@@ -16,15 +17,54 @@ app.get('/dist/main.js.map', (req, res)=> res.sendFile(reactSourceMap));
 const styleSheet = path.join(__dirname, 'styles.css');
 app.get('/styles.css', (req, res)=> res.sendFile(styleSheet));
 
+//GET route /api/movies
+app.get('/api/movies', async(req,res,next)=>{
+  try {
+    const SQL = `
+    SELECT * FROM movies
+    `
+    const response = await client.query(SQL)
+    res.send(response.rows)
+  } catch(error){
+    next(error)
+  }
+})
+
+//Put /api/movies/:id
+app.put('/api/movies/:id', async(req,res,next)=>{
+  try {
+    const SQL = `
+      UPDATE movies
+      SET title = $1, stars = $2
+      WHERE id = $3
+      RETURNING *
+    `
+    const response = await client.query(SQL, [req.body.title, req.body.stars, req.params.id])
+  } catch(error){
+    next(error)
+  }
+})
+
 const init = async()=> {
   await client.connect();
   console.log('connected to database');
   const SQL = `
-    SQL SETUP AND SEED
+    DROP TABLE IF EXISTS movies;
+    CREATE TABLE movies(
+      id SERIAL PRIMARY KEY,
+      title VARCHAR(255),
+      stars INT
+    );
+    INSERT INTO movies (title, stars) VALUES ('28 Days Later', 5);
+    INSERT INTO movies (title, stars) VALUES ('The Mist', 1);
+    INSERT INTO movies (title, stars) VALUES ('Repo Man', 3);
+    INSERT INTO movies (title, stars) VALUES ('Tombstone', 5);
+    INSERT INTO movies (title, stars) VALUES ('Barbie', 4);
   `;
+  await client.query(SQL)
   console.log('create your tables and seed data');
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3100;
   app.listen(port, ()=> {
     console.log(`listening on port ${port}`);
   });
--- src/index.js
+++ src/index.js
@@ -1,9 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios'
 
 const App = ()=> {
+  const [movies, setMovies] = useState([])
+
+  useEffect(()=>{
+    const fetchMovies = async()=>{
+      const data = await axios.get('/api/movies')
+      setMovies(data)
+    }
+    fetchMovies()
+  }, [])
+
+  const raiseRating = async(movie)=>{
+    const newRating = movie.stars + 1
+    const {data} = await axios.put(`/api/movies/${movie.id}`, {title: movie.title, stars: movie.stars})
+    const newMovies = movies.map((movieMap)=>{
+      if (movieMap.id === movie.id){
+        return data
+      } else {
+        return movieMap
+      }
+    })
+    setMovies(newMovies)
+  }
+
+  const lowerRating = async(movie)=>{
+    const newRating = movie.stars - 1
+    const {data} = await axios.put(`/api/movies/${movie.id}`, {title: movie.title, stars: movie.stars})
+    const newMovies = movies.map((movieMap)=>{
+      if (movieMap.id === movie.id){
+        return data
+      } else {
+        return movieMap
+      }
+    })
+    setMovies(newMovies)
+  }
+
   return (
-    <h1>FullStack Template</h1>
+    <div>
+    <h1>Movie Rating Database</h1>
+    <ul>
+      {
+        movies.map((movie)=>{
+          return(
+            <li key={movie.id}>
+              <h2>{movie.title}</h2>
+              <h3>
+                <span>
+                  Rating: {movie.stars}
+                  <button onCLick={()=>{raiseRating(movie)}}>
+                    +
+                  </button>
+                  <button onCLick={()=>{lowerRating(movie)}}>
+                    -
+                  </button>
+                </span>
+              </h3>
+            </li>
+          )
+        })
+      }
+    </ul>
+
+    </div>
   );
 };
 
